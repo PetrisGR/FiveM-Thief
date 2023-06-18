2d27af6bf82da87a21bf3cd03e5348c2901995bb
Stamatis Petris <121623120+PetrisGR@users.noreply.github.com>
2023-06-18 21:27:59 +0300

    Initial Release

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Petris <petrisservices.tebex.io>'
+description 'Advanced Thief Script'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+
+shared_script('config/main.lua')
+
+server_scripts {
+    'config/framework_sv.lua',
+    'server/main.lua'
+}
+
+client_script('client/main.lua')
+
+depedency 'ox_lib'
